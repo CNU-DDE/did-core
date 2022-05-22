258a60bb02a72949e19529c071b59aef9a0099e9
haeramkeem <ewqdsacxz2345@gmail.com>
2022-05-22 23:44:09 +0900

    style: organize tests

--- __tests__/service.test.ts
+++ __tests__/service.test.ts
@@ -1,63 +1,61 @@
 import * as service from '../src/service'
 
-// DID resolving test
-test('DID resolving test', async () => {
-  expect(await service.resolveDID(
-        '0x031ef767936996de95f5be7b36fada08d070b97e85d874ce23e5f9fcbdf7149aa2'
-  )).toBeTruthy();
+describe('Test group #1: DID and DID resolving', () => {
+
+    // DID resolving test
+    test('Should resolve DID', async () => {
+        expect(await service.resolveDID(
+            '0x031ef767936996de95f5be7b36fada08d070b97e85d874ce23e5f9fcbdf7149aa2'
+        )).toBeTruthy();
+    });
+
+    // Wrong DID test
+    test('Should throw error for wrong identifier', () => {
+        expect(async () => {
+            await service.resolveDID('wrong-identifier')
+        }).rejects.toThrowError('Cannot resolve DID');
+    });
+
+    // Wrong Infura project id test
+    test('Should throw error when INFURA_PID is not set', () => {
+        process.env.INFURA_PID = '';
+        expect(() => {
+            service.getInfuraResolver()
+        }).toThrowError('Cannot import Infura project ID');
+    });
+
 });
 
-// Wrong DID test
-test('Wrong DID test', () => {
-  expect(async () => {
-      await service.resolveDID('wrong-identifier')
-  }).rejects.toThrowError('Cannot resolve DID');
+describe('Test group #2: VC test', () => {
+
+    // Create VC test
+    test('Should create VC', async () => {
+        expect(await service.createVC(
+            'did:ethr:0x435df3eda57154cf8cf7926079881f2912f54db4',
+            {
+                degree: {
+                    type: 'BachelorDegree',
+                    name: 'CSE'
+                }
+            },
+            '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
+            'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75'
+        )).toBeTruthy();
+    });
+
 });
 
-// Create VC test
-test('Creating VC test', async () => {
-    expect(await service.createVC(
-        'did:ethr:0x435df3eda57154cf8cf7926079881f2912f54db4',
-        {
-            degree: {
-                type: 'BachelorDegree',
-                name: 'CSE'
-            }
-        },
-        '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
-        'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75'
-    )).toBeTruthy();
-});
+describe('Test group #3: VP test', () => {
 
-// Create VP test
-test('Creating VP test', async () => {
-    const testVC = "eyJhbGciOiJFUzI1NkstUiIsInR5cCI6IkpXVCJ9.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImRlZ3JlZSI6eyJ0eXBlIjoiQmFjaGVsb3JEZWdyZWUiLCJuYW1lIjoiQ1NFIn19fSwic3ViIjoiZGlkOmV0aHI6MHg0MzVkZjNlZGE1NzE1NGNmOGNmNzkyNjA3OTg4MWYyOTEyZjU0ZGI0IiwibmJmIjoxNTYyOTUwMjgyLCJpc3MiOiJkaWQ6ZXRocjoweEYxMjMyRjg0MGYzYUQ3ZDIzRmNEYUE4NGQ2QzY2ZGFjMjRFRmIxOTgifQ.La-maDcP8NXaucFDwSK-rD4DYmcIvBCQa4CA3q-05bCzdHHf6ZSdHQWMJuwn34vIMAl6tBCS992QKrWwEZT5QQA";
+    // Create VP test
+    test('Should create VP', async () => {
+        const testVC = "eyJhbGciOiJFUzI1NkstUiIsInR5cCI6IkpXVCJ9.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImRlZ3JlZSI6eyJ0eXBlIjoiQmFjaGVsb3JEZWdyZWUiLCJuYW1lIjoiQ1NFIn19fSwic3ViIjoiZGlkOmV0aHI6MHg0MzVkZjNlZGE1NzE1NGNmOGNmNzkyNjA3OTg4MWYyOTEyZjU0ZGI0IiwibmJmIjoxNTYyOTUwMjgyLCJpc3MiOiJkaWQ6ZXRocjoweEYxMjMyRjg0MGYzYUQ3ZDIzRmNEYUE4NGQ2QzY2ZGFjMjRFRmIxOTgifQ.La-maDcP8NXaucFDwSK-rD4DYmcIvBCQa4CA3q-05bCzdHHf6ZSdHQWMJuwn34vIMAl6tBCS992QKrWwEZT5QQA";
 
-    expect(await service.createVP(
-        '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
-        'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75',
-        [ testVC ]
-    )).toBeTruthy();
-});
+        expect(await service.createVP(
+            '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
+            'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75',
+            [ testVC ]
+        )).toBeTruthy();
+    });
 
-// Env-related testing
-// Reference: https://stackoverflow.com/a/48042799
-describe('Environment dependent testing', () => {
-  const OLD_ENV = process.env;
-
-  beforeEach(() => {
-    jest.resetModules() // Most important - it clears the cache
-    process.env = { ...OLD_ENV }; // Make a copy
-  });
-
-  afterAll(() => {
-    process.env = OLD_ENV; // Restore old environment
-  });
-
-  test('Should throw error when INFURA_PID is not set', () => {
-      process.env.INFURA_PID = '';
-      expect(() => {
-          service.getInfuraResolver()
-      }).toThrowError('Cannot import Infura project ID');
-  });
 });
