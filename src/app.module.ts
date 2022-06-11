0556b4a89893d100beab5fa23dc7d5bdb243677e
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-11 19:18:57 +0900

    feat: connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import settings from './settings';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: settings.MARIADB_HOST,
+            port: settings.MARIADB_PORT,
+            username: settings.MARIADB_USER,
+            password: settings.MARIADB_PASSWORD,
+            database: settings.MARIADB_DATABASE,
+            entities: [],
+            synchronize: true,
+            autoLoadEntities: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
+
 export class AppModule {}
