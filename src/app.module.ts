8a676b8a700850bfd22b9a5f824dc14af01f30f1
haeramkeem <ewqdsacxz2345@gmail.com>
2022-06-17 21:02:00 +0900

    feat: pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClaimsModule } from './claims/claims.module';
 import { ResumesModule } from './resumes/resumes.module';
+import { LoggerModule } from 'nestjs-pino';
 import Env from './config/env.config';
 
 @Module({
     imports: [
         MongooseModule.forRoot(`mongodb://${Env.get('mongodb.user')}:${Env.get('mongodb.password')}@${Env.get('mongodb.host')}:${Env.get('mongodb.port')}/${Env.get('mongodb.database')}`),
+        LoggerModule.forRoot(),
         ClaimsModule,
         ResumesModule,
     ],
